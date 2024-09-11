e2786fd3010789ae6d3e8fce88ca79458fa7a1dd
Tomoki Saito | 齋藤 智樹 <tomoki.trp.0920@icloud.com>
2024-09-11 20:01:26 +0900

    JOB_TITLE の追加

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Tomoki Saito's Portfolio"
 export const SITE_DESCRIPTION = '齋藤 智樹のポートフォリオサイトです。'
+export const JOB_TITLE = 'Web Developer & UX Designer'
