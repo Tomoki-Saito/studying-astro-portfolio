ac22db323fb532b9ebe9347ce7b1bd26a58db0f9
Tomoki Saito | 齋藤 智樹 <tomoki.trp.0920@icloud.com>
2024-09-11 18:55:42 +0900

    Header, Footer コンポーネントの調整

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = "Tomoki Saito's Portfolio"
+export const SITE_DESCRIPTION = '齋藤 智樹のポートフォリオサイトです。'
